7f371363df1cb322853b15ba21b3989b03b9bbeb
Aaron Pearce <me@aaronpearce.com>
2024-02-14 10:46:55 +1300

    Initial Commit

---
+++ IttyBittyList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IttyBittyList
+//
+//  Created by Aaron Pearce on 14/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IttyBittyList/IttyBittyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  IttyBittyListApp.swift
+//  IttyBittyList
+//
+//  Created by Aaron Pearce on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct IttyBittyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
